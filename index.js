01e0b0da504423bf648b2f437c005a86526755dc
Carol Greene <carolgreene3@gmail.com>
2018-10-27 01:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 //   }
 //
 //   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, 
+//     return `Hi, my name is ${this.name}. I am from ${this.homeState},
 //and I was trained in ${this.training}.`
 //   }
 // }
@@ -46,6 +46,5 @@ BoardMember.prototype.releasePressStatement = function() {
 }
 
 BoardMember.prototype.sayHi = function() {
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, 
-  and I was trained in ${this.training}.`
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
